acc7cefd81081689715a9e7fdb9a3149e32d5bda
amedjuvinao-bit <amedjuvinao@gmail.com>
2026-07-02 22:50:58 -0500

    fix: retirar controlador externo POS

--- frontend/src/admin/pos/PosSalesPage.js
+++ frontend/src/admin/pos/PosSalesPage.js
@@ -1,10 +1,5 @@
 // frontend/src/admin/pos/PosSalesPage.js
 
-import React, { useEffect } from 'react';
 import Page from './PosSalesPage.jsx';
-import { attachPosConfirmController } from './posConfirmSaleDom';
 
-export default function PosSalesPage() {
-  useEffect(() => attachPosConfirmController(), []);
-  return React.createElement(Page);
-}
+export default Page;
