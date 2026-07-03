8fb391ac142e6bb5c8cb91059ba669c602c048c6
amedjuvinao-bit <amedjuvinao@gmail.com>
2026-07-02 22:44:29 -0500

    fix: usar confirmacion estable POS

--- frontend/src/admin/pos/PosSalesPage.js
+++ frontend/src/admin/pos/PosSalesPage.js
@@ -1,5 +1,10 @@
 // frontend/src/admin/pos/PosSalesPage.js
 
+import React, { useEffect } from 'react';
 import Page from './PosSalesPage.jsx';
+import { attachPosConfirmController } from './posConfirmSaleDom';
 
-export default Page;
+export default function PosSalesPage() {
+  useEffect(() => attachPosConfirmController(), []);
+  return React.createElement(Page);
+}
