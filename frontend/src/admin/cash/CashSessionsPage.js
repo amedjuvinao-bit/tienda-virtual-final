1b08ca6bccadf34f82ab3d447cebfe47b7778280
amedjuvinao-bit <amedjuvinao@gmail.com>
2026-07-07 14:38:04 -0500

    feat: activar opcion retiro caja

--- frontend/src/admin/cash/CashSessionsPage.js
+++ frontend/src/admin/cash/CashSessionsPage.js
@@ -1,5 +1,14 @@
+import React from 'react';
 import './cashReportModal.css';
 
 import Page from './CashSessionsPageReport.jsx';
+import CashMovementWithdrawalOptionPatch from './CashMovementWithdrawalOptionPatch.jsx';
 
-export default Page;
+export default function CashSessionsPage() {
+  return (
+    <>
+      <CashMovementWithdrawalOptionPatch />
+      <Page />
+    </>
+  );
+}
