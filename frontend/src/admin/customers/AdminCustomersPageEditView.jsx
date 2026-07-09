4b650f2d32ec26aae05dd71f9ad0426dd1ddfe24
amedjuvinao-bit <amedjuvinao@gmail.com>
2026-07-09 15:37:51 -0500

    feat: activar interfaz seguimiento clientes

--- frontend/src/admin/customers/AdminCustomersPageEditView.jsx
+++ frontend/src/admin/customers/AdminCustomersPageEditView.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import AdminCustomersPageEditable from './AdminCustomersPageEditable.jsx';
+import CustomerFollowUpsVisualPatch from './CustomerFollowUpsVisualPatch.jsx';
 
 export default function AdminCustomersPageEditView() {
-  return React.createElement(AdminCustomersPageEditable);
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(CustomerFollowUpsVisualPatch),
+    React.createElement(AdminCustomersPageEditable)
+  );
 }
